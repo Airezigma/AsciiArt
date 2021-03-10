43ca015d57afd714ada4eb15b140e29b85badf1f
Diego Neri <diego@diegoneri.com>
2021-03-09 21:34:08 -0300

    Corrige indentação do código
    https://rodrigofavaro.com/2016/11/02/o-que-e-indentacao-de-codigo/

--- Program.cs
+++ Program.cs
@@ -5,11 +5,11 @@ namespace AsciiArt
     class Program
     {
         static void Main(string[] args)
-        {Console.WriteLine(" ||  ||");
-        Console.WriteLine(@" \\()//");
- 	    Console.WriteLine(@"//(__)\\");
- 	    Console.WriteLine(" || ||");
-    
+        {
+            Console.WriteLine(" ||  ||");
+            Console.WriteLine(@" \\()//");
+            Console.WriteLine(@"//(__)\\");
+            Console.WriteLine(" || ||");
         }
     }
 }
